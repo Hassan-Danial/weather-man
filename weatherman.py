364086b2e59951257e2a9a6218dd883cc0e70831
Hassan-Danial <hassanjutt1210@gmail.com>
2022-08-01 00:27:16 +0500

    Optimized space complexity by removing Dictionary

--- weatherman.py
+++ weatherman.py
@@ -1,11 +1,10 @@
-from statistics import mean
 from zipfile import ZipFile
 import os
 import getopt
 import sys
 import gc
 
-weather_data = {}
+
 months = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
@@ -20,78 +19,81 @@ def integer_conversion(s):
 
 
 def temperature_humidity_calculator_of_year(year, dir_list):
-    """Search in the weather_data_of_each_day which contain data of every day
-    for our input year data and store the data of that specific year in a list
-    named seperate_data_data
+    """Search through the files names, only open those files which match 
+    with our input and only parse through those files.
+
+    Now the only the data which is use full is considered all data values
+    which are of no use in are program are neglected only values required 
+    in our program are used like at seperate_date_data[1] at index 1 max
+    temperature values are present and at every loop iteration will give 
+    us new value of new day, to find out the maxand min values the data 
+    values are compared with each other,
     """
-    max = float("-inf")
-    min = float("inf")
-    huimidityMax = float("-inf")
+    max_value_in_max_temp = (float("-inf"), "")
+    min_value_in_min_temp = (float("inf"), "")
+    huimidity_max = (float("-inf"), "")
     for file in dir_list:
         if(year in file):
             f = open("./weatherfiles/"+file)
-            for line in f.readlines()[1:]:
-                seperate_date_data = line.rstrip().split(",", 1)
-                weather_data[seperate_date_data[0]
-                             ] = seperate_date_data[1].split(",")
-                maxTemperature = integer_conversion(
-                    weather_data[seperate_date_data[0]][0])
-                minTemperature = integer_conversion(
-                    weather_data[seperate_date_data[0]][2])
-                humidity = integer_conversion(
-                    weather_data[seperate_date_data[0]][6])
-                if(maxTemperature > max):
-                    max = maxTemperature
-                if(minTemperature < min):
-                    min = minTemperature
-                if(humidity > huimidityMax):
-                    huimidityMax = humidity
 
-    date = seperate_date_data[0].split("-")
-    print("Highest Temperature: "+str(max) +
+            for line in f.readlines()[1:]:
+                seperate_date_data = line.rstrip().split(",")[:10]
+
+                max_temperature_value = integer_conversion(
+                    seperate_date_data[1])
+                min_temperature_value = integer_conversion(
+                    seperate_date_data[3])
+                humidity = integer_conversion(
+                    seperate_date_data[7])
+
+                if(max_temperature_value > max_value_in_max_temp[0]):
+                    max_value_in_max_temp = max_temperature_value, seperate_date_data[0]
+                if(min_temperature_value < min_value_in_min_temp[0]):
+                    min_value_in_min_temp = min_temperature_value, seperate_date_data[0]
+                if(humidity > huimidity_max[0]):
+                    huimidity_max = humidity, seperate_date_data[0]
+
+    date = max_value_in_max_temp[1].split("-")
+    print("Highest Temperature: "+str(max_value_in_max_temp[0]) +
           "C"+" "+months[int(date[1])]+" "+date[2])
-    print("Lowest Temperature: "+str(min) +
+    date = min_value_in_min_temp[1].split("-")
+    print("Lowest Temperature: "+str(min_value_in_min_temp[0]) +
           "C"+" "+months[int(date[1])]+" "+date[2])
-    print("Humidity: "+str(huimidityMax) +
+    date = huimidity_max[1].split("-")
+    print("Highest Temperature: "+str(huimidity_max[0]) +
           "%"+" "+months[int(date[1])]+" "+date[2])
-    weather_data.clear()
-    del max
-    del min
-    del huimidityMax
     del seperate_date_data
     gc.collect()
 
 
 def average_temperature_humidity_calculator_of_month(year, month, dir_list):
+    """" As the for loop at every iteration will give us new max and min 
+    temperature values, the addition of all the values and counting the 
+    number of lines in the file will give us the days in month which 
+    also give us the total number of days to find average of temperature
+    """
     maxTemperature = 0
     minTemperature = 0
     humidity = 0
-    counter = 0
+    number_of_days_in_month = 0
     for file in dir_list:
         if(year+"_"+(months[int(month)]) in file):
             f = open("./weatherfiles/"+file)
             for line in f.readlines()[1:]:
-                seperate_date_data = line.rstrip().split(",", 1)
-                weather_data[seperate_date_data[0]
-                             ] = seperate_date_data[1].split(",")
+                seperate_date_data = line.rstrip().split(",")[:10]
                 maxTemperature += integer_conversion(
-                    weather_data[seperate_date_data[0]][0])
+                    seperate_date_data[1])
                 minTemperature += integer_conversion(
-                    weather_data[seperate_date_data[0]][2])
+                    seperate_date_data[3])
                 humidity += integer_conversion(
-                    weather_data[seperate_date_data[0]][7])
-                counter = counter+1
+                    seperate_date_data[8])
+                number_of_days_in_month = number_of_days_in_month+1
             break
 
-    print('Highest Average: '+str(maxTemperature/counter)+'C')
-    print("Lowest Average: "+str(minTemperature/counter)+"C")
-    print("Average Mean Humidity: "+str(humidity/counter)+"%")
-    weather_data.clear()
+    print('Highest Average: '+str(maxTemperature/number_of_days_in_month)+" "+"C")
+    print("Lowest Average: "+str(minTemperature/number_of_days_in_month)+" "+"C")
+    print("Average Mean Humidity: "+str(humidity/number_of_days_in_month)+" "+"%")
     del seperate_date_data
-    del maxTemperature
-    del minTemperature
-    del humidity
-    del counter
     gc.collect()
 
 
@@ -99,11 +101,10 @@ def bar_chart_of_temperature_everyday_of_month(year, month, dir_list):
     """ After searching through all the files names in dir_list, just open that
     file which contains monthly data of input month. After seperate the first 
     line containing weather data variables names and filtering only data values.
-    Speciifed the index number of values that we need in list conatin data vales and get all data.
     """
-    minTemperatureValuesOfAMonth = 0
-    maxTemperatureValuesOfAMonth = 0
-    day = 1
+    min_temperature_value_of_a_month = 0
+    max_temperature_value_of_a_month = 0
+    number_of_days_in_month = 1
     day_with_leading_zeros = ""
     month_name = months[int(month)]
     for file in dir_list:
@@ -111,32 +112,26 @@ def bar_chart_of_temperature_everyday_of_month(year, month, dir_list):
             f = open("./weatherfiles/"+file)
             print(year+" "+month_name)
             for line in f.readlines()[1:]:
-                seperate_date_data = line.rstrip().split(",", 1)
-                weather_data[seperate_date_data[0]
-                             ] = seperate_date_data[1].split(",")
-                maxTemperatureValuesOfAMonth = integer_conversion(
-                    weather_data[seperate_date_data[0]][0])
-                minTemperatureValuesOfAMonth = integer_conversion(
-                    weather_data[seperate_date_data[0]][2])
+                seperate_date_data = line.rstrip().split(",")[:10]
+                max_temperature_value_of_a_month = integer_conversion(
+                    seperate_date_data[1])
+                min_temperature_value_of_a_month = integer_conversion(
+                    seperate_date_data[3])
 
                 # Fill the spaces of single digit with zeros so the bar chart is uniform
-                day_with_leading_zeros = str(day).zfill(2)
+                day_with_leading_zeros = str(number_of_days_in_month).zfill(2)
                 print("\033[1;32;31m", day_with_leading_zeros, end=" ")
-                print("+" * maxTemperatureValuesOfAMonth, end=" ")
-                print(maxTemperatureValuesOfAMonth, end="C\n")
+                print("+" * max_temperature_value_of_a_month,
+                      "\033[0;0;0m", end="")
+                print(max_temperature_value_of_a_month, end="C\n")
 
                 print("\033[1;32;34m", day_with_leading_zeros, end=" ")
-                print("+" * minTemperatureValuesOfAMonth, end=" ")
-                print(minTemperatureValuesOfAMonth, end="C\n")
+                print("+" * min_temperature_value_of_a_month,
+                      "\033[0;0;0m", end="")
+                print(min_temperature_value_of_a_month, end="C\n")
 
-                day += 1
+                number_of_days_in_month += 1
             break
-    weather_data.clear()
-    del minTemperatureValuesOfAMonth
-    del maxTemperatureValuesOfAMonth
-    del day_with_leading_zeros
-    del day
-    del month_name
     del seperate_date_data
     gc.collect()
 
@@ -147,9 +142,9 @@ def bar_chart_of_temperature_everyday_of_month_in_one_line(year, month, dir_list
     calculations on List, after performing calculations deleting the used 
     variables, data structures and free memeory using Garbage Collector
     """
-    minTemperatureValueOfAMonth = 0
-    maxTemperatureValueOfAMonth = 0
-    day = 1
+    min_temperature_value_of_a_month = 0
+    max_temperature_value_of_a_month = 0
+    number_of_days_in_month = 1
     day_with_leading_zeros = ""
     month_name = months[int(month)]
     for file in dir_list:
@@ -157,43 +152,35 @@ def bar_chart_of_temperature_everyday_of_month_in_one_line(year, month, dir_list
             f = open("./weatherfiles/"+file)
             print(year+" "+month_name)
             for line in f.readlines()[1:]:
-                seperate_date_data = line.rstrip().split(",", 1)
-                weather_data[seperate_date_data[0]
-                             ] = seperate_date_data[1].split(",")
-                maxTemperatureValueOfAMonth = integer_conversion(
-                    weather_data[seperate_date_data[0]][0])
-                minTemperatureValueOfAMonth = integer_conversion(
-                    weather_data[seperate_date_data[0]][2])
+                seperate_date_data = line.rstrip().split(",")[:10]
+                max_temperature_value_of_a_month = integer_conversion(
+                    seperate_date_data[1])
+                min_temperature_value_of_a_month = integer_conversion(
+                    seperate_date_data[3])
 
                 # Fill the spaces of single digit with zeros so the bar chart is uniform
-                day_with_leading_zeros = str(day).zfill(2)
+                day_with_leading_zeros = str(number_of_days_in_month).zfill(2)
                 # Color the text using the ANSI Escape Sequence
                 print(day_with_leading_zeros, "\033[0;0m", end="")
                 print("\033[1;32;31m" + "+" *
-                      maxTemperatureValueOfAMonth + "\033[0;0m", end="")
+                      max_temperature_value_of_a_month + "\033[0;0m", end="")
 
                 print("\033[1;32;34m" + "+" *
-                      minTemperatureValueOfAMonth + "\033[0;0m", end=" ")
-                print(maxTemperatureValueOfAMonth, end="C-")
-                print(minTemperatureValueOfAMonth, end="C\n")
+                      min_temperature_value_of_a_month + "\033[0;0m", end=" ")
+                print(max_temperature_value_of_a_month, end="C-")
+                print(min_temperature_value_of_a_month, end="C\n")
 
-                day += 1
+                number_of_days_in_month += 1
             break
-    weather_data.clear()
-    del minTemperatureValueOfAMonth
-    del maxTemperatureValueOfAMonth
-    del day_with_leading_zeros
-    del month_name
     del seperate_date_data
-    del day
     gc.collect()
 
 
 def main():
-    """Calling Functions according to given Command Line arguments using 
-    'getopt' Library and defining the actions with each of the argument 
+    """Calling Functions according to given Command Line arguments using
+    'getopt' Library and defining the actions with each of the argument
     after recieving
-    After calling the specific argument oriented function the returned 
+    After calling the specific argument oriented function the returned
     results are printed here in the specified format
     Example: Highest Temperature: 32C on June 18, here Result with the
     data 'June 18'
@@ -228,17 +215,22 @@ def main():
             print("")
         elif opt in ["-c"]:
             if(arg.split("/")[1][0] == '0'):
+                # Spliting the month string to get month and year
                 month = int(arg.split("/")[1])
                 year = arg.split("/")[0]
                 bar_chart_of_temperature_everyday_of_month_in_one_line(
                     year, str(month), dir_list)
                 print("")
             else:
+                # Spliting the month string to get month and year
                 month = (arg.split("/")[1])
                 year = arg.split("/")[0]
                 bar_chart_of_temperature_everyday_of_month(
                     year, month, dir_list)
                 print("")
+        else:
+            print(
+                "Command is in Wrong Format.\nWrite command like this : 'python3 weatherman.py -p ./weatherfiles.zip -e 2004 -a 2005/6 -c 2011/7'")
     del year
     del month
     del argv
