d25a6864d3c42a1d2c28444f65e5d07ca8105565
Hassan-Danial <hassanjutt1210@gmail.com>
2022-08-03 12:35:51 +0500

    Formatted Code with pure pep8 conventions

--- test_weatherman.py
+++ test_weatherman.py
@@ -1,6 +1,15 @@
-import weatherman
+"""Each test case is testing the function with all values avaible in database
+(weatherfiles.zip) according to their argument requirements. First function
+which takes a year as input for values calculation for entire year, tests are
+implemented in this way that the function is being tested for all posible
+years present in database so every possible input is test. Second function
+is also implemented the same with values being tested for all possible month
+in a year present in the database.
+"""
 import os
 from zipfile import ZipFile
+import io
+import weatherman
 
 
 with ZipFile("/Users/hassandanial/W/weatherfiles.zip", 'r') as zip_file:
@@ -14,41 +23,43 @@ YEARS = []
 YEAR_WITH_MONTH = []
 # Filtering the years present in databse to test values for each year.
 for file in DIRECTORIES_LIST:
-    if(len(file.split("_")) > 2):
+    if len(file.split("_")) > 2:
         year = (file.split("_")[2])
-        if(year in YEARS):
+        if (year in YEARS):
             year_in_list = YEARS.index((year))
         else:
             YEARS.append(year)
             year_in_list = YEARS.index((year))
-# Filtering the months with year present in databse to test every possible month input for each year.
+# Filtering the months with year present in databse to test every
+# possible month input for each year.
 for file in DIRECTORIES_LIST:
-    if(len(file.split("_")) > 2):
+    if len(file.split("_")) > 2:
         year = (file.split("_")[2])
         month = (file.split("_")[3].split(".")[0])
-        if((year, month) in YEAR_WITH_MONTH):
+        if ((year, month) in YEAR_WITH_MONTH):
             year_and_month_in_list = YEAR_WITH_MONTH.index((year, month))
         else:
             YEAR_WITH_MONTH.append((year, month))
             year_and_month_in_list = YEAR_WITH_MONTH.index((year, month))
 
-del year
-
 
 def test_temperature_humidity_calculator_of_year():
+    """calculating maximum, minimum and humidity values and second function
+    calculating average maximum, average minimum and average mean humidty
+    values. The first functionwith maximum, minimum and humidity values
+    calculate only for one entire year
+    """
     max_value_in_max_temp = float("-inf")
     min_value_in_min_temp = float("inf")
     humidity_max = float("-inf")
-    temperature_humidity_values_of_one_year = []
-    for year in YEARS:
-        for file in DIRECTORIES_LIST:
-            if(str(year) in file):
-
-                read_object = open("./weatherfiles/"+file)
-
+    temp_humid_values_of_one_year = []
+    for one_year in YEARS:
+        for each_file in DIRECTORIES_LIST:
+            if (str(one_year) in each_file):
+                read_object = io.open(
+                    "./weatherfiles/" + each_file, encoding="utf-8")
                 for line in read_object.readlines()[1:]:
-                    seperate_date_data = weatherman.get_convert_read_file_content_lines_to_list(
-                        line)
+                    seperate_date_data = weatherman.get_line_from_file(line)
 
                     max_temperature_value = weatherman.convert_to_int(
                         seperate_date_data[1])
@@ -56,48 +67,62 @@ def test_temperature_humidity_calculator_of_year():
                         seperate_date_data[3])
                     humidity = weatherman.convert_to_int(
                         seperate_date_data[7])
-                    if(max_temperature_value > max_value_in_max_temp):
+                    if (max_temperature_value > max_value_in_max_temp):
                         max_value_in_max_temp = max_temperature_value
-                    if(min_temperature_value < min_value_in_min_temp):
+                    if (min_temperature_value < min_value_in_min_temp):
                         min_value_in_min_temp = min_temperature_value
-                    if(humidity > humidity_max):
+                    if (humidity > humidity_max):
                         humidity_max = humidity
         # assert will keep the test running until it recieves false until then
         # assert keep assuring that values are equal
-        temperature_humidity_values_of_one_year = weatherman.get_temperature_humidity_of_a_year(
-            year, DIRECTORIES_LIST)
-        assert temperature_humidity_values_of_one_year[0][0] == max_value_in_max_temp and temperature_humidity_values_of_one_year[
-            1][0] == min_value_in_min_temp and temperature_humidity_values_of_one_year[2][0] == humidity_max
+        temp_humid_values_of_one_year = weatherman.get_temp_humidity_of_a_year(
+            one_year, DIRECTORIES_LIST)
+        assert (
+            temp_humid_values_of_one_year[0][0] == max_value_in_max_temp) and (
+            temp_humid_values_of_one_year[1][0] == min_value_in_min_temp) and (
+            temp_humid_values_of_one_year[2][0] == humidity_max)
         max_value_in_max_temp = float("-inf")
         min_value_in_min_temp = float("inf")
         humidity_max = float("-inf")
 
 
 def test_average_temperature_humidity_calculator_of_month():
-    max_temperature_values_sum = 0
-    min_temperature_values_sum = 0
+    """Testing all function average_temperature_humidity_calculator_of_month
+    with all possible value from data values (weatherfiles.zip) that could
+    be given as input
+    """
+    max_temperature_sum = 0
+    min_temperature_sum = 0
     humidity = 0
     number_of_days_in_month = 0
     for year_and_month in YEAR_WITH_MONTH:
-        for file in DIRECTORIES_LIST:
-            if(year_and_month[0]+"_"+year_and_month[1] in file):
-                read_object = open("./weatherfiles/"+file)
+        for each_file in DIRECTORIES_LIST:
+            if (year_and_month[0] + "_" + year_and_month[1] in each_file):
+                read_object = io.open(
+                    "./weatherfiles/" + each_file, encoding="utf-8")
                 for line in read_object.readlines()[1:]:
-                    seperate_date_data = weatherman.get_convert_read_file_content_lines_to_list(
+                    seperate_date_data = weatherman.get_line_from_file(
                         line)
-                    max_temperature_values_sum += weatherman.convert_to_int(
+                    max_temperature_sum += weatherman.convert_to_int(
                         seperate_date_data[1])
-                    min_temperature_values_sum += weatherman.convert_to_int(
+                    min_temperature_sum += weatherman.convert_to_int(
                         seperate_date_data[3])
                     humidity += weatherman.convert_to_int(
                         seperate_date_data[8])
-                    number_of_days_in_month = number_of_days_in_month+1
+                    number_of_days_in_month = number_of_days_in_month + 1
                 break
-        average_max_min_temperature_and_humidity = weatherman.get_average_temperature_humidity_of_month(
-            year_and_month[0], weatherman.MONTHS.index(year_and_month[1]), DIRECTORIES_LIST)
-        assert (average_max_min_temperature_and_humidity[0]) == (max_temperature_values_sum/number_of_days_in_month) and (
-            average_max_min_temperature_and_humidity[1]) == (min_temperature_values_sum/number_of_days_in_month) and (average_max_min_temperature_and_humidity[2]) == (humidity/number_of_days_in_month)
-        max_temperature_values_sum = 0
-        min_temperature_values_sum = 0
+        avg_max_min_temp_and_humid = weatherman.get_avg_temp_humid_of_month(
+            year_and_month[0], weatherman.MONTHS.index(
+                year_and_month[1]), DIRECTORIES_LIST)
+        assert (
+            avg_max_min_temp_and_humid[0]) == (
+                max_temperature_sum / number_of_days_in_month) and (
+            avg_max_min_temp_and_humid[1]) == (
+                min_temperature_sum / number_of_days_in_month) and (
+            avg_max_min_temp_and_humid[2]) == (
+                humidity /
+            number_of_days_in_month)
+        max_temperature_sum = 0
+        min_temperature_sum = 0
         humidity = 0
         number_of_days_in_month = 0
--- weatherman.py
+++ weatherman.py
@@ -1,26 +1,39 @@
+"""Weatherman is a console based applicaton which generates reports regarding
+maximum, minimum temperature and humidity, also generate horizontal bar graph
+of maximum amd minimum temperature values of every day of a month. There are
+two type of horizontal bar gaph program generates.
+Bar graph of maximum and minimum value for every day of a month
+Bar graph of maximum and minimum value for every day of a month
+(Bars are shelfed side by side)
+Two reports it generates:
+Maximum, minimum temperature and humidity report
+Average maximum, average minimum temperature and average mean humidity report
+"""
 from zipfile import ZipFile
 import os
 import getopt
 import sys
 import gc
+import io
 
 
 MONTHS = [None, 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
           'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 
-def convert_to_int(s):
+def convert_to_int(data_value):
     """As all of the data is in textual format and to perform calculations we
     have to convert it to integer format, this function handles the Null
     values in the data and convert them to 0 so no disturbed mathematical
     calculations
     """
-    return int(s) if s else 0
+    return int(data_value) if data_value else 0
 
 
-def print_horizontal_bar_chart_with_max_min_values_in_different_lines(number_of_days_in_month,
-                                                                      max_temperature_value_of_a_month,
-                                                                      min_temperature_value_of_a_month):
+def print_horizontal_bar_chart_with_max_min_values_in_diff_lines(
+        number_of_days_in_month,
+        max_temperature_value_of_a_month,
+        min_temperature_value_of_a_month):
     """Prints Horizontal Bar Graph for given data values for every day of a
     month. ANSI escape sequnce is used to color the text. No ending sequence
     is used, becouse we need to switch colors at every line.
@@ -38,13 +51,10 @@ def print_horizontal_bar_chart_with_max_min_values_in_different_lines(number_of_
     print(min_temperature_value_of_a_month, end="C\n")
 
 
-def get_print(number):
-    return ("+"*number)
-
-
-def print_horizontal_bar_chart_with_max_min_values_in_same_line(number_of_days_in_month,
-                                                                max_temperature_value_of_a_month,
-                                                                min_temperature_value_of_a_month):
+def print_horizontal_bar_chart_with_max_min_values_in_same_line(
+        number_of_days_in_month,
+        max_temperature_value_of_a_month,
+        min_temperature_value_of_a_month):
     """Printing the bar graph with maximum and minimum values in same line
     using '+'. Colored the '+' using ANSI escape sequence. Staring and
     edding sequences are used to have two colors in the same line.
@@ -61,7 +71,7 @@ def print_horizontal_bar_chart_with_max_min_values_in_same_line(number_of_days_i
     print(min_temperature_value_of_a_month, end="C\n")
 
 
-def get_temperature_humidity_of_a_year(year, directories_list):
+def get_temp_humidity_of_a_year(year, directories_list):
     """Search through the files names, only open those files which match
     with our input and only parse through those files.
 
@@ -72,34 +82,40 @@ def get_temperature_humidity_of_a_year(year, directories_list):
     us new value of new day, to find out the maxand min values the data
     values are compared with each other,
     """
-    max_value_in_maximum_temperature_weather_variable = (float("-inf"), "")
-    min_value_in_minimum_temperature_weather_variable = (float("inf"), "")
+    max_value_in_max_temp_weather_variable = (float("-inf"), "")
+    min_value_in_min_temp_weather_variable = (float("inf"), "")
     max_value_in_huimidity_weather_variable = (float("-inf"), "")
     for file_name in directories_list:
-        if(year in file_name):
-            file_read_object = open("./weatherfiles/"+file_name)
+        if (year in file_name):
+            file_read_object = io.open(
+                "./weatherfiles/" + file_name, encoding="utf-8")
             for line in file_read_object.readlines()[1:]:
                 per_day_data_values = line.rstrip().split(",")[:10]
 
-                max_temperature_value = convert_to_int(
+                max_temp_value = convert_to_int(
                     per_day_data_values[1])
                 min_temperature_value = convert_to_int(
                     per_day_data_values[3])
                 humidity = convert_to_int(
                     per_day_data_values[7])
 
-                if(max_temperature_value > max_value_in_maximum_temperature_weather_variable[0]):
-                    max_value_in_maximum_temperature_weather_variable = max_temperature_value, per_day_data_values[
-                        0]
-                if(min_temperature_value < min_value_in_minimum_temperature_weather_variable[0]):
-                    min_value_in_minimum_temperature_weather_variable = min_temperature_value, per_day_data_values[
-                        0]
-                if(humidity > max_value_in_huimidity_weather_variable[0]):
-                    max_value_in_huimidity_weather_variable = humidity, per_day_data_values[0]
-    return [(max_value_in_maximum_temperature_weather_variable), (min_value_in_minimum_temperature_weather_variable), (max_value_in_huimidity_weather_variable)]
+                if (max_temp_value
+                        > max_value_in_max_temp_weather_variable[0]):
+                    max_value_in_max_temp_weather_variable = (
+                        max_temp_value, per_day_data_values[0])
+                if (min_temperature_value
+                        < min_value_in_min_temp_weather_variable[0]):
+                    min_value_in_min_temp_weather_variable = (
+                        min_temperature_value, per_day_data_values[0])
+                if humidity > max_value_in_huimidity_weather_variable[0]:
+                    max_value_in_huimidity_weather_variable = (
+                        humidity, per_day_data_values[0])
+    return [(max_value_in_max_temp_weather_variable),
+            (min_value_in_min_temp_weather_variable),
+            (max_value_in_huimidity_weather_variable)]
 
 
-def get_average_temperature_humidity_of_month(year, month, directories_list):
+def get_avg_temp_humid_of_month(year, month, directories_list):
     """" As the for loop at every iteration will give us new max and min
     temperature values, the addition of all the values and counting the
     number of lines in the file will give us the days in month which
@@ -110,11 +126,10 @@ def get_average_temperature_humidity_of_month(year, month, directories_list):
     humidity = 0
     number_of_days_in_month = 0
     for file in directories_list:
-        if(year+"_"+MONTHS[int(month)] in file):
-            f = open("./weatherfiles/"+file)
-            for line in f.readlines()[1:]:
-                per_day_data_values = get_convert_read_file_content_lines_to_list(
-                    line)
+        if (year + "_" + MONTHS[int(month)] in file):
+            read_object = io.open("./weatherfiles/" + file, encoding="utf-8")
+            for line in read_object.readlines()[1:]:
+                per_day_data_values = get_line_from_file(line)
                 max_temperature_values_sum += convert_to_int(
                     per_day_data_values[1])
                 minimum_temperature_values_sum += convert_to_int(
@@ -123,44 +138,50 @@ def get_average_temperature_humidity_of_month(year, month, directories_list):
                     per_day_data_values[8])
                 number_of_days_in_month += 1
             break
-    return [(max_temperature_values_sum/number_of_days_in_month), (minimum_temperature_values_sum/number_of_days_in_month), (humidity/number_of_days_in_month)]
+    return [(max_temperature_values_sum / number_of_days_in_month),
+            (minimum_temperature_values_sum / number_of_days_in_month),
+            (humidity / number_of_days_in_month)]
 
 
 def bar_chart_of_temperature_everyday_of_month(year, month, directories_list):
-    """ After searching through all the files names in directories_list, just open that
-    file which contains monthly data of input month. After seperate the first
-    line containing weather data variables names and filtering only data values.
+    """ After searching through all the files names in directories_list,
+    just open that file which contains monthly data of input month.
+    After seperate the first line containing weather data variables names
+    and filtering only data values.
     """
     number_of_days_in_month = 1
     month_name = MONTHS[int(month)]
     for file in directories_list:
-        if(year+"_"+month_name in file):
-            file_read_object = open("./weatherfiles/"+file)
+        if (year + "_" + month_name in file):
+            file_read_object = io.open(
+                "./weatherfiles/" + file, encoding="utf-8")
             for line in file_read_object.readlines()[1:]:
-                per_day_data_values = get_convert_read_file_content_lines_to_list(
+                per_day_data_values = get_line_from_file(
                     line)
-                # Fill the spaces of single digit with zeros so the bar chart is uniform
-                print_horizontal_bar_chart_with_max_min_values_in_different_lines(
-                    number_of_days_in_month,
-                    convert_to_int(per_day_data_values[1]),
-                    convert_to_int(per_day_data_values[3]))
+                # Fill the spaces of single digit with zeros so the bar chart
+                # is uniform
+                print_horizontal_bar_chart_with_max_min_values_in_diff_lines(
+                    number_of_days_in_month, convert_to_int(
+                        per_day_data_values[1]), convert_to_int(
+                        per_day_data_values[3]))
                 number_of_days_in_month += 1
             break
     del per_day_data_values
     gc.collect()
 
 
-def get_convert_read_file_content_lines_to_list(line):
+def get_line_from_file(line):
     """This module is used to convert the line present in the text files to
     list so we can access data values saperately acoording to our calculation.
 
-    Only first 10 value are selected every line, becouse all other data is 
+    Only first 10 value are selected every line, becouse all other data is
     irrelevent and we don't need them in our calculations so why store them.
     """
     return line.rstrip().split(",")[:10]
 
 
-def bar_chart_of_temperature_everyday_of_month_in_one_line(year, month, directories_list):
+def bar_chart_of_temp_everyday_of_month_in_one_line(
+        year, month, directories_list):
     """Searching for the year and month in files list and only opening that
     specific file and saving weather data vlues in List and perform
     calculations on List, after performing calculations deleting the used
@@ -169,10 +190,11 @@ def bar_chart_of_temperature_everyday_of_month_in_one_line(year, month, director
     number_of_days_in_month = 1
     month_name = MONTHS[int(month)]
     for file_name in directories_list:
-        if(year+"_"+month_name) in file_name:
-            file_read_object = open("./weatherfiles/"+file_name)
+        if (year + "_" + month_name) in file_name:
+            file_read_object = io.open(
+                "./weatherfiles/" + file_name, encoding="utf-8")
             for line in file_read_object.readlines()[1:]:
-                per_day_data_values = get_convert_read_file_content_lines_to_list(
+                per_day_data_values = get_line_from_file(
                     line)
                 print_horizontal_bar_chart_with_max_min_values_in_same_line(
                     number_of_days_in_month,
@@ -191,11 +213,14 @@ def command_line_arguments():
     argv = sys.argv[1:]
     # Exception Handling if the given command line arguments are incorrect
     try:
-        opts, args = getopt.getopt(argv, "p:e:a:c:")
-    except:
-        print("Please give arguments in sequence, you should give path command in this format:")
+        opts = getopt.getopt(argv, "p:e:a:c:")[0]
+    except IndexError:
+        print("Please give arguments in sequence, you should give path",
+              end="")
+        print("command in this format:", end="")
         print(
-            "'python3 weatherman.py -p {path} -e 2005 -c 2005/11 -c 2005/05 -a 2011/5'")
+            "'python3 weatherman.py -p {path} -e 2005 -c 2005/11 -c 2005/05")
+        print("-a 2011/5'", end="")
 
     # Obtaining the list of arguments as List and then search of each argument
     # using defined flags like '-p' '-a', '-e' and '-c' and then performing
@@ -208,23 +233,24 @@ def command_line_arguments():
                     zip_file.extractall()
                     # Selecting only txt files as there are other files in the
                     # databse causing problems in dataflow
-                directories_list = [file_in_folder for file_in_folder in os.listdir(
-                    "./weatherfiles") if file_in_folder.endswith('.txt')]
+                directories_list = [file_in_folder for file_in_folder in
+                                    os.listdir("./weatherfiles") if
+                                    file_in_folder.endswith('.txt')]
             elif opt in ['-e']:
-                print(get_temperature_humidity_of_a_year(arg, directories_list))
+                print(get_temp_humidity_of_a_year(arg, directories_list))
                 print("")
             elif opt in ['-a']:
                 month = (arg.split("/")[1])
                 year = arg.split("/")[0]
-                print(get_average_temperature_humidity_of_month(
+                print(get_avg_temp_humid_of_month(
                     year, (month), directories_list))
                 print("")
             elif opt in ["-c"]:
-                if(arg.split("/")[1][0] == '0'):
+                if (arg.split("/")[1][0] == '0'):
                     # Spliting the month string to get month and year
                     month = int(arg.split("/")[1])
                     year = arg.split("/")[0]
-                    print(bar_chart_of_temperature_everyday_of_month_in_one_line(
+                    print(bar_chart_of_temp_everyday_of_month_in_one_line(
                         year, str(month), directories_list))
                     print("")
                 else:
@@ -235,13 +261,21 @@ def command_line_arguments():
                         year, month, directories_list)
                     print("")
             else:
+                print("Command is in Wrong Format.")
+                print("Write command like this:", end="")
                 print(
-                    "Command is in Wrong Format.\nWrite command like this : 'python3 weatherman.py -p ./weatherfiles.zip -e 2004 -a 2005/6 -c 2011/7'")
-    except:
-        print(
-            "\033[0;32;31m", "Please Recheck Your command \nYour command must be in this format :\n'python3 weatherman.py -p {path} -e 2005 -c 2005/11 -c 2005/05 -a 2011/5'")
-        print("The Months should be immediately after backward slash like: '2005/6', also write valid month")
-        print("You must provide the zip folder location after '-p' indicator\n\n")
+                    "'python3 weatherman.py -p ./weatherfiles.zip -e 2004",
+                    end="")
+                print("-a 2005/6 -c 2011/7'", end="")
+    except IndexError:
+        print("\033[0;32;31m", "Please Recheck Your command \n", end="")
+        print("Your command must be in this format :\n", end="")
+        print("'python3 weatherman.py -p {path} -e 2005 -c 2005/11 -c", end="")
+        print("2005/05 -a 2011/5'", end="")
+        print("The Months should be immediately after backward ", end="")
+        print("slash like '2005/6',also write valid month", end="")
+        print("You must provide the zip folder location after '-p' indicator")
+        print("\n\n")
     del year
     del month
     del argv
